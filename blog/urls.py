b277b3ed50627f5695b656f397c8eb33753bbefa
Sahil Rajput <sahil.rajput.0196@gmail.com>
2019-06-24 22:39:59 +0530

    Add files via upload

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from . import views
+from .models import Post
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    #path('upvote/<int:pk>', views.upvote, name = "upvote"),
+    #path('downvote/<int:pk>', views.downvote, name = "downvote")
+]
